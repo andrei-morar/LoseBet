4b0a7da01672dad729e4332f720896a69c1cb096
AndreiMoraru <nila.urarom@gmail.com>
2026-04-22 22:41:39 +0300

    Register+login modificat si creata o pagina dupa log

--- LoseBet.Core/Models/IdentityValidator.cs
+++ LoseBet.Core/Models/IdentityValidator.cs
@@ -1,29 +1,53 @@
-﻿public static class IdentityValidator
+﻿using System;
+using System.Linq;
+
+namespace LoseBet.Core.Models // Asigură-te că folosești namespace-ul corect
 {
-    public static bool ValidateCNP(string cnp)
+    public static class IdentityValidator
     {
-        if (cnp.Length != 13 || !cnp.All(char.IsDigit)) return false;
-
-        // Extragem data nașterii din CNP pentru verificare vârstă
-        // Format CNP: S AA LL ZZ ...
-        int s = int.Parse(cnp[0].ToString());
-        int aa = int.Parse(cnp.Substring(1, 2));
-        int ll = int.Parse(cnp.Substring(3, 2));
-        int zz = int.Parse(cnp.Substring(5, 2));
-
-        int anComplet = (s == 1 || s == 2) ? 1900 + aa : 2000 + aa;
-
-        try
+        // Acum cerem și dataNasteriiDeclarata ca parametru
+        public static bool ValidateCNP(string cnp, DateTime dataNasteriiDeclarata)
         {
-            DateTime dataNasterii = new DateTime(anComplet, ll, zz);
-            int varsta = DateTime.Now.Year - dataNasterii.Year;
-            if (dataNasterii > DateTime.Now.AddYears(-varsta)) varsta--;
+            if (string.IsNullOrWhiteSpace(cnp) || cnp.Length != 13 || !cnp.All(char.IsDigit))
+                return false;
 
-            return varsta >= 18; // Validăm doar dacă are 18+ ani
-        }
-        catch
-        {
-            return false;
+            // Extragem componentele datei din CNP
+            int s = int.Parse(cnp[0].ToString());
+            int aa = int.Parse(cnp.Substring(1, 2));
+            int ll = int.Parse(cnp.Substring(3, 2));
+            int zz = int.Parse(cnp.Substring(5, 2));
+
+            // Stabilim anul complet (1,2 = 1900+ ; 5,6 = 2000+)
+            int anComplet = (s == 1 || s == 2) ? 1900 + aa : 2000 + aa;
+
+            try
+            {
+                // Construim data de naștere reală ascunsă în CNP
+                DateTime dataNasteriiCnp = new DateTime(anComplet, ll, zz);
+
+                // --- NOU: 1. Verificăm dacă data declarată coincide cu cea din CNP ---
+                // Folosim .Date pentru a ignora orele/minutele care ar putea veni din WPF
+                if (dataNasteriiCnp.Date != dataNasteriiDeclarata.Date)
+                {
+                    return false; // Nu se potrivesc!
+                }
+
+                // --- 2. Verificăm dacă are 18 ani împliniți ---
+                int varsta = DateTime.Now.Year - dataNasteriiCnp.Year;
+
+                // Dacă nu și-a serbat încă ziua anul acesta, scădem un an
+                if (DateTime.Now < dataNasteriiCnp.AddYears(varsta))
+                {
+                    varsta--;
+                }
+
+                return varsta >= 18;
+            }
+            catch
+            {
+                // Pică aici dacă CNP-ul conține o dată imposibilă (ex: 30 Februarie)
+                return false;
+            }
         }
     }
 }
